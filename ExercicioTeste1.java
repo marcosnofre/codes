a10aba45459c1c82a9440e886dcaa71910c83193
marcosnofre <marcos.nofre@hotmail.com>
2021-03-23 15:14:35 -0300

    exercicio2

---
+++ ExercicioTeste1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ExercicioTeste1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        Scanner entrada = new Scanner(System.in);
+    }
+}
